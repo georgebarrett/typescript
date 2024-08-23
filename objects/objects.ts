ed128776f45f17010aa29c90423dd923e4c7d0cf
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-23 16:40:20 +0100

    practice using arrays in typescript

--- objects/objects.ts
+++ objects/objects.ts
@@ -6,7 +6,20 @@
 
 const person = {
     name: 'george',
-    age: 36
+    age: 36,
+    // string[] = typescript syntax for an array of strings
+    hobbies: ['writing', 'running']
 };
+
+// empty variable but typescript is expecting a 
+let favouriteHobbies: string[];
+favouriteHobbies = ['writing'];
+
+console.log(person.name)
+
+// hobby is identified as a string. therefore i can autocomplete in-built methods like .toLowerCase()
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
 
 console.log(person);
