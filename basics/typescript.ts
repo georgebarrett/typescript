4419a14e2d961d2c56314cebac7e844f3739b8a7
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-23 14:49:01 +0100

    add function expanded. still practicing the basics of types and how they work

--- basics/typescript.ts
+++ basics/typescript.ts
@@ -1,10 +1,22 @@
-const add = (number1: number, number2: number) => {
-    console.log(typeof number1);
-    return number1 + number2;
+const add = (number1: number, number2: number, showResult: boolean, phrase: string) => {
+    // console.log(typeof number1);
+    // return number1 + number2;
+
+    const result = number1 + number2
+    if (showResult) {
+        // can't concatonate strings before numbers. they all become strings. can with variables
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'result is: '
 
-const result = add(number1, number2);
-console.log(result);
+
+add(number1, number2, printResult, resultPhrase);
+// this console log is not necessary due to the if-statement
+// console.log(result);
