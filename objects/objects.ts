06c093257f8b545691aa8901d29f1f131906fa44
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-23 16:20:59 +0100

    playing with typescript objects and type assignment

---
+++ objects/objects.ts
@@ -0,0 +1,12 @@
+// in typescript, objects can be used for type assignment
+// const objectTypes: {
+//     name: string;
+//     age: number
+// }
+
+const person = {
+    name: 'george',
+    age: 36
+};
+
+console.log(person);
