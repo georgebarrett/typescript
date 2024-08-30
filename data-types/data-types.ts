ae4238512476df23753ed83e10f774450396b944
George Barrett <georgeaustinbarrett@gmail.com>
2024-08-30 13:28:43 +0100

    playing with enums

--- data-types/data-types.ts
+++ data-types/data-types.ts
@@ -4,19 +4,37 @@
 //     age: number
 // }
 
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    // a tuple! bsasically an array with a defined structure. there must be two elements 1. is a number 2. is a string
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     // a tuple! bsasically an array with a defined structure. there must be two elements 1. is a number 2. is a string
+//     role: [number, string];
+// } = {
+//     name: 'george',
+//     age: 36,
+//     // string[] = typescript syntax for an array of strings
+//     hobbies: ['writing', 'running'],
+//     role: [2, 'author']
+// };
+
+enum Role {
+    USER,
+    ADMIN,
+    SUPER_ADMIN
+};
+
+const person = {
     name: 'george',
     age: 36,
     // string[] = typescript syntax for an array of strings
     hobbies: ['writing', 'running'],
-    role: [2, 'author']
+    role: Role.SUPER_ADMIN
 };
+
+if (person.role === Role.SUPER_ADMIN) {
+    console.log('hola mundo');
+}
 
 // empty variable but typescript is expecting a 
 let favouriteHobbies: string[];
