9ba8b1af9434b930b1227d98dd4bcc6f427b298a
George Barrett <georgeaustinbarrett@gmail.com>
2024-09-06 13:53:44 +0100

    spread operators and objects

--- analytics/analytics.ts
+++ analytics/analytics.ts
@@ -4,3 +4,11 @@ const activeHobbies = ['RUNNING'];
 // the spread operator pulls all the elements of 'hobbies' into 'activeHobbies'
 activeHobbies.push(...hobbies);
 console.log(hobbies, activeHobbies);
+
+const planet = {
+    name: 'Saturn',
+    number: 7
+};
+
+// key value pairs are pulled out of the 'planet' object. usually to be added to another object
+const copiedPlanet = { ...planet };
