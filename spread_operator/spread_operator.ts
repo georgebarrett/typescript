b44cab148cf0ef166612d24a581e73e814572362
geab <georgeaustinbarrett@gmail.com>
2024-11-10 16:35:41 +0000

    destructuring arrays

--- spread_operator/spread_operator.ts
+++ spread_operator/spread_operator.ts
@@ -3,7 +3,7 @@ const activeHobbies = ['RUNNING'];
 
 // the spread operator pulls all the elements of 'hobbies' into 'activeHobbies' preventing a nested array
 activeHobbies.push(...hobbies);
-console.log(hobbies, activeHobbies);
+// console.log(hobbies, activeHobbies);
 
 const planet = {
     name: 'Saturn',
@@ -12,3 +12,10 @@ const planet = {
 
 // key value pairs are pulled out of the 'planet' object. creates a perfect copy and not just a pointer
 const copiedPlanet = { ...planet };
+
+// destructuring the hobbies array
+// the destructured elements increment through the hobbies array
+// the rest of the elements par 0 and 1 will be stored in a new array called remainingHobbies
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
