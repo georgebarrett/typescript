ac192cf61056bbb1b72f9f4c33426480d544ad7f
geab <georgeaustinbarrett@gmail.com>
2024-11-10 16:05:26 +0000

    rest paramters logic moved to a fresh directory

--- analytics/analytics.ts
+++ analytics/analytics.ts
@@ -12,15 +12,3 @@ const planet = {
 
 // key value pairs are pulled out of the 'planet' object. creates a perfect copy and not just a pointer
 const copiedPlanet = { ...planet };
-
-// rest parameters
-// 'numbers' is just a placeholder. it is a spread operator to allow a flexible amount of arguments
-// the spread operator does need to be told what to expect
-const addingAgain = (...numbers: number[]) => {
-    return numbers.reduce((currentResult, currentValue) => {
-        return currentResult + currentValue;
-    }, 0)
-};
-
-const addedNumbers = addingAgain(1, 2, 3, 4, 5, 6, 7);
-console.log(addedNumbers);
